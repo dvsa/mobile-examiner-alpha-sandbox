32de346c1f798a491144bcb4adfd476476e8d5bd
Unknown <frederico02@hotmail.com>
2018-03-21 16:54:38 +0000

    ionics network plugin works fine

--- offline-test-submission/src/app/app.module.ts
+++ offline-test-submission/src/app/app.module.ts
@@ -1,30 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { Network } from "@ionic-native/network";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Network,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- offline-test-submission/src/pages/home/home.ts
+++ offline-test-submission/src/pages/home/home.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, ToastController } from "ionic-angular";
+import { Network } from "@ionic-native/network";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    private toast: ToastController,
+    private network: Network
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
+  displayNetworkUpdate(connectionState: string) {
+    let networkType = this.network.type;
+    this.toast
+      .create({
+        message: `You are now ${connectionState}`,
+        duration: 3000,
+        position: "top"
+      })
+      .present();
   }
 
+  ionViewDidEnter() {
+    this.network.onConnect().subscribe(
+      data => {
+        console.log(data);
+        this.displayNetworkUpdate(data.type);
+      },
+      error => console.error(error)
+    );
+
+    this.network.onDisconnect().subscribe(
+      data => {
+        console.log(data);
+        this.displayNetworkUpdate(data.type);
+      },
+      error => console.error(error)
+    );
+  }
 }
